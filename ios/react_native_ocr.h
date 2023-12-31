1f324a6735fcd8be30afc8112d0773212fe9f38c
allenyan <allen.yan7@foxmail.com>
2023-12-31 08:37:05 +0800

    update ios

---
+++ ios/react_native_ocr.h
@@ -0,0 +1,12 @@
+//
+//  react_native_ocr.h
+//  react-native-ocr
+//
+//  Created by gzqyl on 2023/12/31.
+//
+
+#ifndef react_native_ocr_h
+#define react_native_ocr_h
+
+
+#endif /* react_native_ocr_h */
